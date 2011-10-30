962d9f97a77cf7e27efcb17038f625dcefb4ba1a
David Taylor <david@cloudartisan.com>
2011-10-30 23:41:43 +1100

    Cleaned up

--- lib/lowrpm.rb
+++ lib/lowrpm.rb
@@ -1,13 +1,14 @@
 require 'active_resource'
 require 'new_relic_api'
+require 'lowrpm/version'
 
 
-class LowRpm
+module LowRpm
   VERSION = '1.0.0'
 
-	def initialize(api_key, application_name)
-		NewRelicApi.api_key = api_key
-		NewRelicApi.reset!
-		NewRelicApi::Account.find(:first).applications(:params => {:conditions => {:name => application_name}})
-	end
+  def initialize(api_key, application_name)
+    NewRelicApi.api_key = api_key
+    NewRelicApi.reset!
+    NewRelicApi::Account.find(:first).applications(:params => {:conditions => {:name => application_name}})
+  end
 end
